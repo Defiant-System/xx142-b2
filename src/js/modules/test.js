704d5257d03b37843cfcacccd668d9314ac7f50d
Hakan Bilgin <hbi99@hotmail.com>
2024-02-05 20:37:03 +0100

    key ui update

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -5,5 +5,7 @@ let Test = {
 		// setTimeout(() => Game.start(), 100);
 		// setTimeout(() => Game.loadLevel(8), 4000);
 		// setTimeout(() => Game.pause(), 10000);
+
+		// setTimeout(() => APP.content.data({ show: "level" }), 200);
 	}
 };
